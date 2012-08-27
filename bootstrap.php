5afb6a19448de022ba108d9fe845d31652c75434
unknown <Itako@.(none)>
2012-08-27 05:25:33 +0200

    * Added Twig support * Separeted views and controllers

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,9 @@ $loader = require_once __DIR__ . '/vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
 
 $app['root_dir'] = __DIR__;
 
--- src/PRE/Controller/ContactController.php
+++ src/PRE/Controller/ContactController.php
@@ -8,22 +8,134 @@ class ContactController
 {
     public function indexAction(Application $app)
     {
-        include $app['root_dir'] . "/index.php";
+        $db = @mysql_connect($app['config']['database']['host'], $app['config']['database']['username'], $app['config']['database']['password']) or die('Can\'t connect do database');
+        @mysql_select_db($app['config']['database']['name']) or die('The database selected does not exists');
+
+        $query = 'SELECT * FROM contacts ORDER BY lastname';
+        $rs = mysql_query($query);
+
+        if (!$rs)
+        {
+            die_with_error(mysql_error(), $query);
+        }
+
+        $contacts = array();
+        while ($row = mysql_fetch_assoc($rs))
+        {
+            $contacts[] = $row;
+        }
+
+        mysql_free_result($rs);
+        mysql_close($db);
+
+        return $app['twig']->render("contact/index.twig", array('contacts' => $contacts));
     }
 
     public function editAction(Application $app)
     {
-        include $app['root_dir'] . "/edit.php";
+        $errors = null;
+        if (!$_GET['id'])
+        {
+            die('Some error occured!!');
+        }
+
+        $db = @mysql_connect($app['config']['database']['host'], $app['config']['database']['username'], $app['config']['database']['password']) or die('Can\'t connect do database');
+        @mysql_select_db($app['config']['database']['name']) or die('The database selected does not exists');
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            $errors = validate(array('id', 'firstname', 'lastname', 'phone'), $_POST);
+
+            if (count($errors) == 0)
+            {
+                $query = sprintf("UPDATE contacts set firstname = '%s', 
+                                                                          lastname = '%s',
+                                                                          phone = '%s', 
+                                                                          mobile = '%s' WHERE id = %s", mysql_real_escape_string($_POST['firstname']), mysql_real_escape_string($_POST['lastname']), mysql_real_escape_string($_POST['phone']), mysql_real_escape_string($_POST['mobile']), mysql_real_escape_string($_POST['id'])
+                );
+
+                $rs = mysql_query($query);
+
+                if (!$rs)
+                {
+                    die_with_error(mysql_error(), $query);
+                }
+
+                header('Location: ' . $app['url_generator']->generate('index'));
+                exit;
+            }
+        }
+        else
+        {
+            $query = sprintf('SELECT * FROM contacts WHERE id = %s', mysql_real_escape_string($_GET['id']));
+
+            $rs = mysql_query($query);
+
+            if (!$rs)
+            {
+                die_with_error(mysql_error(), $query);
+            }
+
+            $row = mysql_fetch_assoc($rs);
+        }
+
+        mysql_close($db);
+
+        return $app['twig']->render("contact/edit.twig", array("contact" => $row, "errors" => $errors));
     }
 
     public function newAction(Application $app)
     {
-        include $app['root_dir'] . "/new.php";
+        $errors = null;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            $errors = validate(array('firstname', 'lastname', 'phone'), $_POST);
+
+            if (count($errors) == 0)
+            {
+                $db = @mysql_connect($app['config']['database']['host'], $app['config']['database']['username'], $app['config']['database']['password']) or die('Can\'t connect do database');
+                @mysql_select_db($app['config']['database']['name']) or die('The database selected does not exists');
+
+                $query = sprintf("INSERT INTO contacts (firstname, lastname, phone, mobile) VALUES ('%s', '%s', '%s', '%s')", mysql_real_escape_string($_POST['firstname']), mysql_real_escape_string($_POST['lastname']), mysql_real_escape_string($_POST['phone']), mysql_real_escape_string($_POST['mobile'])
+                );
+
+                $rs = mysql_query($query);
+
+                if (!$rs)
+                {
+                    die_with_error(mysql_error(), $query);
+                }
+
+                mysql_close($db);
+
+                header('Location: ' . $app['url_generator']->generate('index'));
+                exit;
+            }
+        }
+        return $app['twig']->render("contact/edit.twig", array('errors' => $errors));
     }
 
     public function removeAction(Application $app)
     {
-        include $app['root_dir'] . "/remove.php";
+        if (!$_GET['id'])
+        {
+            die('Some error occured!!');
+        }
+
+        $db = @mysql_connect($app['config']['database']['host'], $app['config']['database']['username'], $app['config']['database']['password']) or die('Can\'t connect do database');
+        @mysql_select_db($app['config']['database']['name']) or die('The database selected does not exists');
+
+        $query = sprintf('DELETE FROM contacts where ID = %s', mysql_real_escape_string($_GET['id']));
+
+        if (!mysql_query($query))
+        {
+            die_with_error(mysql_error(), $query);
+        }
+
+        mysql_close($db);
+
+        header('Location: ' . $app['url_generator']->generate('index'));
+        exit;
     }
 
 }
